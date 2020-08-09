02833f0d31e75eb7dbff47e87768391c089f9be0
Evangelos Giannakosian <vgiannakosian@pavla.gr>
2020-08-09 09:35:53 +0300

    Created simple counter component

---
+++ my-app/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h2>The count is: {{ count }}</h2>
+
+    <p>
+      <button @click="count++">Increment</button>
+    </p>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: "Counter",
+    data() {
+      return {
+        count: 0
+      }
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -2,17 +2,17 @@
   <div id="app">
 
     <message>Hello there!</message>
-
+    <counter></counter>
   </div>
 </template>
 
 <script>
 
   import Message from "../components/Message";
-
+  import Counter from "../components/Counter";
   export default {
     name: 'app',
-    components:{Message},
+    components:{Message,Counter},
     data() {
       return {}
     }
